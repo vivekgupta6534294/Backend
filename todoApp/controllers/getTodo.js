3d2312e975af5b95e335586cc9c857b6b9cd5904
Vivek Gupta <Vivekgupta6534294@gmail.com>
2025-10-07 16:01:48 +0530

    GetToDoByID Updated

--- todoApp/controllers/getTodo.js
+++ todoApp/controllers/getTodo.js
@@ -24,7 +24,41 @@ exports.getTodo=async(req,res)=>{
 
 
         })
-        
+    }
+}
 
+exports.getTodoById=async(req,res)=>{
+    try{
+        // extract to do item basis on id
+        const id=req.params.id;
+        const todo=await Todo.findById({_id:id})
+        // data for given id not found
+        if(!todo){
+            return res.status(404).json({
+                success:false,
+                message:"No Data found with given id",
+            })
+        }
+        res.status(200).json(
+            {
+                success:true,
+                data:todo,
+                message:`Todo ${id} data successfully feteched`
+            }
+        )
+       
+
+    }
+    catch(err){
+        console.err(err);
+        res.status(500)
+        .json({
+            success:false,
+            data:"Internal Server Error",
+            error:err.message,
+
+
+        });
+       
     }
 }
--- todoApp/routes/todo.js
+++ todoApp/routes/todo.js
@@ -5,9 +5,10 @@ const router=express.Router();
 // import the controller 
 
 const {createTodo}= require("../controllers/createTodo");
-const {getTodo}= require("../controllers/getTodo");
+const {getTodo,getTodoById}= require("../controllers/getTodo");
 
 // define API routes
 router.post("/createTodo",createTodo);// is path ko controller se map krwa dia
 router.get("/getTodos",getTodo);
+router.get("/getTodos/:id",getTodoById);
 module.exports=router;
