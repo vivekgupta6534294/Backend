f51fb60c865b8c6040d5a487cdaa254fc28bd55a
Vivek Gupta <Vivekgupta6534294@gmail.com>
2025-10-07 16:28:51 +0530

    deleteTodo is also completed

---
+++ todoApp/controllers/deleteTodo.js
@@ -0,0 +1,25 @@
+// import the model
+const Todo = require("../models/Todo");
+
+exports.deleteTodo=async(req,res)=>{
+    try{
+         const {id}=req.params;
+         await Todo.findByIdAndDelete(id);
+         res.json({
+            success:true,
+            message:"Todo Deleted",
+         })
+        
+
+    }
+    catch(err){
+        console.error(err);
+        res.status(500)
+        .json({
+            success:false,
+            data : 'Internal Server Error',
+            message:err.message,
+        })
+
+    }
+}
--- todoApp/routes/todo.js
+++ todoApp/routes/todo.js
@@ -7,11 +7,15 @@ const router=express.Router();
 const {createTodo}= require("../controllers/createTodo");
 const {getTodo,getTodoById}= require("../controllers/getTodo");
 const {updateTodo}=require("../controllers/updateTodo");
+const {deleteTodo}=require("../controllers/deleteTodo");
+
+
 
 // define API routes
 router.post("/createTodo",createTodo);// is path ko controller se map krwa dia
 router.get("/getTodos",getTodo);
 router.get("/getTodos/:id",getTodoById);
 router.put("/updateTodo/:id",updateTodo);
+router.delete("/deleteTodo/:id",deleteTodo);
 
 module.exports=router;
